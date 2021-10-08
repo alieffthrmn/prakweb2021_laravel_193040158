754d4782264d1abc3b9cf55cefff563baf3d7c78
Alief Fathurrohman <fathurrohmanalief@gmail.com>
2021-10-08 13:25:20 +0700

    structuring folder, routing and make views #video3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view(
+        'about',
+        [
+            "name" => "Alief Fathurrohman",
+            "email" => "fathurrohmanalief@gmail.com",
+            "image" => "alief.png"
+        ]
+    );
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
