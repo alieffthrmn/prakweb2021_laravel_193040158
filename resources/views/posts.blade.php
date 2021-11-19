df616725af8e1cd675469c6b3191074064509afb
Alief Fathurrohman <fathurrohmanalief@gmail.com>
2021-11-19 07:41:43 +0700

    searching & pagination #video13

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,43 +1,56 @@
 @extends('layouts.main')
 
-
 @section('container')
-  <h1 class="mb-5">{{ $title }}</h1>
+  <h1 class="mb-3 text-center">{{ $title }}</h1>
 
-  @if ($posts->count())  
-    <div class="card mb-3">
-      <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
-      <div class="card-body text-center">
-        <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
-        <p>
-          <small class="text-muted">By. 
-            <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> 
-            in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
-            {{ $posts[0]->created_at->diffForHumans() }}
-        </small>
-      </p>
-        <p class="card-text">{{ $posts[0]->excerpt }}</p>
-        <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
-      </div>
+  <div class="row justify-content-center mb-3">
+    <div class="col-md-6">
+      <form action="/blog">
+      @if (request('category'))
+          <input type="hidden" name="category" value="{{ request('category') }}">
+      @endif
+      @if (request('author'))
+          <input type="hidden" name="author" value="{{ request('author') }}">
+      @endif
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+          <button class="btn btn-dark" type="submit">Search</button>
+        </div>
+      </form>
     </div>
-  @else
-    <p class="text-center fs-4">No Post Found.</p>
-  @endif
+  </div>
 
-  
+  @if ($posts->count())
+  <div class="card mb-3">
+    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+    <div class="card-body text-center">
+      <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+      <p>
+        <small class="text-muted">By. 
+          <a href="/blog?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> 
+          in <a href="/blog?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+          {{ $posts[0]->created_at->diffForHumans() }}
+      </small>
+    </p>
+      <p class="card-text">{{ $posts[0]->excerpt }}</p>
+      <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
+    </div>
+  </div>
+
+
   <div class="container">
     <div class="row">
       @foreach ($posts->skip(1) as $post)
       <div class="col-md-4 mb-3">
         <div class="card">
           <div class="position-absolute bg-dark px-3 py-2 text-white rounded-bottom opacity-75">
-            <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+            <a href="/blog?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
           <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
           <div class="card-body">
             <h5 class="card-title">{{ $post->title }}</h5>
             <p>
               <small class="text-muted">By. 
-                <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
+                <a href="/blog?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
                 {{ $post->created_at->diffForHumans() }}
             </small>
           </p>
@@ -50,5 +63,12 @@
     </div>
   </div>
 
+  @else
+    <p class="text-center fs-4">No posts found.</p>
+  @endif
+
+  <div class="d-flex justify-content-center">
+    {{ $posts->links() }}
+  </div>
 
 @endsection
