64265ff608913dbd9cf1c72139ae2259c9999dba
Alief Fathurrohman <fathurrohmanalief@gmail.com>
2021-12-02 23:18:43 +0700

    view login & registration #video14

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-
 use App\Models\Category;
-use App\Models\User;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -66,3 +67,7 @@ Route::get('/categories', function () {
 //         'posts' => $author->posts->load('category', 'author'),
 //     ]);
 // });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
